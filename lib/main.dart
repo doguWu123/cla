dda7fc36aa2359ecdba09531b1d9ae752a6c3530
neev123 <neev123dev@gmail.com>
2020-12-15 12:01:11 +0530

    removed debug banner, resized container and added one more container

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         home: Scaffold(
             backgroundColor: Colors.white,
             body: Center(
@@ -13,21 +14,24 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 Container(
-                  height: 150.0,
-                  width: 300.0,
+                  height: 35.0,
+                  width: 550.0,
                   color: Colors.transparent,
                   child: Container(
                       decoration: BoxDecoration(
                           color: Colors.teal,
                           borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      child: new Center(
-                        child: new Text(
-                          "Salut c'est français",
-                          style: TextStyle(color: Colors.white, fontSize: 22),
-                          textAlign: TextAlign.right,
-                        ),
-                      )),
+                              BorderRadius.all(Radius.circular(10.0))),),
+                ),
+                Container(
+                  height: 50.0,
+                  width: 500.0,
+                  color: Colors.transparent,
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.teal,
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0))),),
                 ),
               ],
             ))));
